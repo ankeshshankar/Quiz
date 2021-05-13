edb837e451c0a8de260a5c119a2164071ba411a5
prashantkumar1521998@gmail.com <prashantkumar1521998@gmail.com>
2021-05-13 19:12:42 +0530

    first commit

---
+++ app/src/main/java/com/example/quiz/Physics.kt
@@ -0,0 +1,11 @@
+package com.example.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Physics : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_physics)
+    }
+}
